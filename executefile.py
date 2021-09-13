dcce06031bcfd812c63411df63c8cb422bee60a6
Artemy <artemiy.morozov.00@mail.ru>
2021-09-14 01:18:47 +0300

    14.09.21

--- executefile.py
+++ executefile.py
@@ -1,6 +1,24 @@
+import datetime
 from models import *
 
-with db:
-    db.create_tables([Human, Teacher, Student, Group, Subject])
 
-print('gotovo')
+def create_human(full_name, birth_year, birth_month, birth_day):
+    return Human(full_name=full_name, birth_date=datetime.date(birth_year, birth_month, birth_day)).save()
+
+
+def create_subject(title):
+    return Subject(title=title).save()
+
+
+def create_teacher(full_name, birth_year, birth_month, birth_day, subject_id):
+    create_human(full_name, birth_year, birth_month, birth_day)
+    return Teacher(
+        full_name=full_name, birth_date=datetime.date(birth_year, birth_month, birth_day), subject_id=subject_id
+    ).save()
+
+
+with db:
+
+    Human.delete().where(Human.id == 1000).execute()
+
+print('ok')
